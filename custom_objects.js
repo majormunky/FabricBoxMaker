6377473b819c4e94497c1cd7020a504fc97dd296
Josh Bright <josh.bright@gmail.com>
2021-04-06 17:34:17 -0700

    Added a custom objects file to hold custom fabric objects, first one being a Page Rect.  This will represent a webpage that we can use to diagram a workflow

---
+++ custom_objects.js
@@ -0,0 +1,36 @@
+let PageRect = fabric.util.createClass(fabric.Rect, {
+	type: 'pageRect',
+	initialize: function(options) {
+		options || (options = {});
+		this.callSuper("initialize", options);
+		this.set("label", options.label || "<missing label>");
+		this.set("fill", "white");
+		this.set("stroke", "black");
+	},
+	toObject: function() {
+		return fabric.util.object.extend(this.callSuper('toObject'), {
+			label: this.get("label"),
+		});
+	},
+	_render: function(ctx) {
+		this.callSuper("_render", ctx);
+		console.log(this)
+		console.log(-this.width / 2)
+		console.log(this.width / 2)
+		ctx.font = "20px Helvetica";
+		ctx.fillStyle = "#333";
+		ctx.fillText(
+			this.label,
+			-34,
+			-this.height / 2 + 26
+		);
+
+		ctx.lineWidth = 1;
+		ctx.fillStyle = "#000";
+		ctx.beginPath();
+		ctx.moveTo(-this.width / 2, -this.height / 2 + 40);
+		ctx.lineTo(this.width / 2, -this.height / 2 + 40);
+		ctx.closePath();
+		ctx.stroke();
+	}
+});
