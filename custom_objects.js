2bf702c49ba574fbcaf639af02edb499c844e2e1
Josh Bright <josh.bright@gmail.com>
2021-04-06 17:36:58 -0700

    Disabled the fabric controls on a PageRect

--- custom_objects.js
+++ custom_objects.js
@@ -6,6 +6,7 @@ let PageRect = fabric.util.createClass(fabric.Rect, {
 		this.set("label", options.label || "<missing label>");
 		this.set("fill", "white");
 		this.set("stroke", "black");
+		this.set("hasControls", false);
 	},
 	toObject: function() {
 		return fabric.util.object.extend(this.callSuper('toObject'), {
